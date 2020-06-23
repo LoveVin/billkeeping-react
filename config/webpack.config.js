8542c84660ec0257fc5f8411e58491c7aaf44b5d
twinklelixn <xiaonanli@163.com>
2020-06-23 22:33:57 +0800

    高亮路由 svg 图标

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -336,7 +336,7 @@ module.exports = function(webpackEnv) {
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
                 resolvePluginsRelativeTo: __dirname,
-                
+
               },
               loader: require.resolve('eslint-loader'),
             },
@@ -352,7 +352,11 @@ module.exports = function(webpackEnv) {
               test: /\.svg$/,
               use: [
                 { loader: 'svg-sprite-loader', options: {} },
-                { loader: 'svgo-loader', options: {} }
+                { loader: 'svgo-loader', options: {
+                    plugins: [
+                      {removeAttrs: {attrs: 'fill'}},
+                    ]
+                  } }
               ]
             },
             // "url" loader works like "file" loader except that it embeds assets
@@ -376,7 +380,7 @@ module.exports = function(webpackEnv) {
                 customize: require.resolve(
                   'babel-preset-react-app/webpack-overrides'
                 ),
-                
+
                 plugins: [
                   [
                     require.resolve('babel-plugin-named-asset-import'),
@@ -418,7 +422,7 @@ module.exports = function(webpackEnv) {
                 cacheDirectory: true,
                 // See #6846 for context on why cacheCompression is disabled
                 cacheCompression: false,
-                
+
                 // Babel sourcemaps are needed for debugging into node_modules
                 // code.  Without the options below, debuggers like VSCode
                 // show incorrect code and set breakpoints on the wrong lines.
