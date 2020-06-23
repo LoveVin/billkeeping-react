590b3358d678499e831121375868b6e4a0da18c7
twinklelixn <xiaonanli@163.com>
2020-06-23 20:31:06 +0800

    抽离 Icon 组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,7 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-
-require( 'icons/money.svg');
-require('icons/tag.svg');
-require('icons/chart.svg');
+import Icon from './Icon';
 
 const NavWrapper = styled.div`
   line-height: 24px;
@@ -32,21 +29,15 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#tag" />
-                    </svg>
+                    <Icon name="tag"/>
                     <Link to="/tags">标签</Link>
                 </li>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#money" />
-                    </svg>
+                    <Icon name="money"/>
                     <Link to="/money">记账</Link>
                 </li>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#chart" />
-                    </svg>
+                    <Icon name="chart"/>
                     <Link to="/statistics">统计</Link>
                 </li>
             </ul>
