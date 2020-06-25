bf74f2ea7357c6805d7e0e0d6cc48ef061158f95
twinklelixn <xiaonanli@163.com>
2020-06-25 12:41:38 +0800

    NumberPad 功能 wip(work in process)

--- src/components/Money/NumberPadSection.tsx
+++ src/components/Money/NumberPadSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   > .output{
@@ -43,5 +44,54 @@ const NumberPadSection = styled.section`
     }
   }
 `;
+
+const NumberPadSection: React.FC = () => {
+    const [output, setOutput] = useState('0');
+    const onClickButtonWrapper = (e: React.MouseEvent) => {
+        const text = (e.target as HTMLButtonElement).textContent;
+        if (text === null) {
+            return;
+        }
+        switch (text) {
+            case '.':
+                break;
+            case '删除':
+                break;
+            case '清空':
+                break;
+            case 'OK':
+                break;
+            default:
+                if (output === '0') {
+                    setOutput(text);
+                } else {
+                    setOutput(output + text);
+                }
+        }
+    };
+    return (
+        <Wrapper>
+            <div className="output">
+                {output}
+            </div>
+            <div className="pad clearfix" onClick={onClickButtonWrapper}>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>删除</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>清空</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button className="ok">OK</button>
+                <button className="zero">0</button>
+                <button>.</button>
+            </div>
+        </Wrapper>
+    );
+};
 
 export {NumberPadSection};
