596ff472c7ff9e36bc029429bea85766a5f40c17
twinklelixn <xiaonanli@163.com>
2020-06-27 14:47:49 +0800

    抽离出单独的 Button.tsx

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -4,6 +4,7 @@ import {useTags} from '../useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
 import {Link} from 'react-router-dom';
+import {Button} from '../components/Button';
 
 const TagList = styled.ol`
   font-size: 16px;
@@ -17,21 +18,8 @@ const TagList = styled.ol`
       display: flex;
       justify-content: space-between;
       align-items: center;
-      > .icon{
-        width: 16px;
-        height: 16px;
-      }
     }
   }
-`;
-
-const Button = styled.button`
-  font-size: 18px;
-  border: none;
-  padding: 8px 12px;
-  background: #ffaf3e;
-  color: white;
-  border-radius: 4px;
 `;
 
 const Center = styled.div`
