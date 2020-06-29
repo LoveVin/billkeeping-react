72d5ea784e59e70560ce24975eb420530af6be85
twinklelixn <xiaonanli@163.com>
2020-06-29 11:48:17 +0800

    添加部署命令

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+yarn build &&
+
+cd build &&
+
+git init &&
+
+git add . &&
+
+git commit -m 'deploy' &&
+
+git remote add origin git@github.com:LoveVin/billkeeping-react-website.git &&
+
+git push -u origin master -f
+
+cd -
+
